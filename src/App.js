ece627f36c59e3d777e64d5c0dc7102324806bf3
Michel Ceddia <cedmicheldia@gmail.com>
2023-08-28 15:41:48 +0200

    Rename layout in layout1 to fix error

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
-import Layout from "./layouts/layout";
+import Layout from "./layouts/layout1";
 import Accueil from "./pages/accueil";
 import Contact from "./pages/contact";
 // Le Club
