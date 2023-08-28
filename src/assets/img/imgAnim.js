e63669222d449911c09bac5849c7e13f3435c944
Michel Ceddia <cedmicheldia@gmail.com>
2023-08-28 19:15:07 +0200

    Change size of img

--- src/assets/img/imgAnim.js
+++ src/assets/img/imgAnim.js
@@ -1,11 +1,11 @@
-import badminton from "./imganimations/01_badminton_1920.jpg";
-import venteFromageVin from "./imganimations/02_venteFromageVin.jpg";
-import championnats from "./imganimations/03_tennis-court_1920.jpg";
-import tournoiInterne from "./imganimations/04_tennis-raquettes_1920.jpg";
-import programme from "./imganimations/05_programme.jpg";
-import pickleball from "./imganimations/06_pickleball_1920.jpg";
-import ecoleTennis from "./imganimations/07_ecoleTennis_1920.jpg";
-import soireeClub from "./imganimations/08_soireeClub_1920.png";
+import badminton from "./imganimations/01_badminton.png";
+import venteFromageVin from "./imganimations/02_venteFromageVin.png";
+import championnats from "./imganimations/03_tennis-court.png";
+import tournoiInterne from "./imganimations/04_tennis-raquettes.png";
+import programme from "./imganimations/05_programme.png";
+import pickleball from "./imganimations/06_pickleball.png";
+import ecoleTennis from "./imganimations/07_ecoleTennis.png";
+import soireeClub from "./imganimations/08_soireeClub.png";
 
 const imgAnim = [
   badminton,
